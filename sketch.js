84ce1c2dd87468906c01fc7938ce82c0764a4cad
AaryakCreator <82655626+AaryakCreator@users.noreply.github.com>
2021-09-18 18:32:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour;
+
+var bg = "sunrise.png";
+
+function preload() {
+    getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+
+    Engine.update(engine);
+
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+    }
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var time = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+ 
+    //change the data in JSON format and store it in variable responseJSON
+    var responseJSON = await time.json();
+
+    
+    //fetch datetime from responseJSON
+    var dateTime = responseJSON.datetime;
+    
+
+    // slice the datetime to extract hour
+    hour = dateTime.slice(11,13);
+    console.log(hour);
+
+    
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
+    }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
+}
